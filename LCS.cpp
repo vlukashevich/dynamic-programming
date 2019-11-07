79057120a6eff8a3afecae4c9ff667a1af3af81a
Vladislav Lukashevich <43114166+vlukashevich@users.noreply.github.com>
2019-11-07 18:40:31 +0300

    Update LCS.cpp

--- LCS.cpp
+++ LCS.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int n, m, A[1001][1001], a[1000], b[1000];
 
+// recursive reconstruction of LCS
 void LCS (int i, int j) {
   if (A[i][j] == 0) return;
 
@@ -19,20 +20,21 @@ void LCS (int i, int j) {
 }
 
 int main() {
+  // read the first sequence
   cin >> n;
-  
   for (int i = 0; i < n; i++) {
     cin >> a[i];
     A[i][0] = 0;
   }
   
+  // read the second sequence
   cin >> m;
-  
   for (int j = 0; j < m; j++)  {
     cin >> b[j];
     A[0][j] = 0;
   }
-
+  
+  // calculate the length of the LCS with recurrent formula
   for (int i = 1; i <= n; i++) {
     for (int j = 1; j <= m; j++) {
       if (a[i - 1] == b[j - 1]) {
@@ -43,6 +45,6 @@ int main() {
     }
   }
   
-  cout << A[n][m];
+  cout << A[n][m] << endl;
   LCS(n, m);
 }

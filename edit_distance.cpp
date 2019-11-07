355778f49cad58f89e3b0b6b0f2ff6dba33317f6
Vladislav Lukashevich <43114166+vlukashevich@users.noreply.github.com>
2019-11-07 19:01:51 +0300

    Add files via upload

---
+++ edit_distance.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+string a, b;
+int DP[1001][1001], n, m;
+int main() {
+  cin >> a >> b;
+  n = a.size();
+  m = b.size();
+
+  for (int i = 0; i <= n; i++) DP[i][0] = i;
+  for (int j = 0; j <= m; j++) DP[0][j] = j;
+
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= m; j++) {
+      if (a[i - 1] == b[j - 1]) {
+        DP[i][j] = DP[i - 1][j - 1];
+      } else {
+        DP[i][j] = 1 + min(min(DP[i - 1][j], DP[i][j - 1]), DP[i - 1][j - 1]);
+      }
+    }
+  }
+  cout << DP[n][m];
+}

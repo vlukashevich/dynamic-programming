d9d842b330d29f9d1e4f76c5730164003c819e49
Vladislav Lukashevich <43114166+vlukashevich@users.noreply.github.com>
2019-11-07 18:37:33 +0300

    Add files via upload

---
+++ LCS.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int n, m, A[1001][1001], a[1000], b[1000];
+
+void LCS (int i, int j) {
+  if (A[i][j] == 0) return;
+
+  if (a[i - 1] == b[j - 1]) {
+    LCS(i - 1, j - 1);
+    cout << a[i - 1] << " ";
+  } else {
+    if (A[i - 1][j] > A[i][j - 1]) {
+      LCS(i - 1, j);
+    } else {
+      LCS(i, j - 1);
+    }
+  }
+}
+
+int main() {
+  cin >> n;
+  
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+    A[i][0] = 0;
+  }
+  
+  cin >> m;
+  
+  for (int j = 0; j < m; j++)  {
+    cin >> b[j];
+    A[0][j] = 0;
+  }
+
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= m; j++) {
+      if (a[i - 1] == b[j - 1]) {
+        A[i][j] = 1 + A[i - 1][j - 1];
+      } else {
+        A[i][j] = max(A[i - 1][j], A[i][j - 1]);
+      }
+    }
+  }
+  
+  cout << A[n][m];
+  LCS(n, m);
+}
